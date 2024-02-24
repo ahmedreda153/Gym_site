438c42616c6d5ab2871e2d9ae49cf17e78b7074f
Reda <ahmedreda5338@gmail.com>
2024-02-24 15:40:50 +0200

    1- check responsive for all pages 2- handle navbar 3- make new footer 4- handle some css code in all pages

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 const navbar_toggler = document.querySelector(".navbar-toggler");
 const navbar_nav = document.getElementById("navbar-nav");
-const profileIconContainer1 = document.getElementById("profile-icon-container1");
-const profileIconContainer2 = document.getElementById("profile-icon-container2");
 
 navbar_toggler.addEventListener("click", () => {
     navbar_nav.classList.toggle("d-none");
@@ -12,18 +10,12 @@ function handleWindowSize() {
     if (window.innerWidth > 992) {
         navbar_toggler.hidden = true;
         navbar_nav.classList.remove("navbar-collapse", "d-none");
-        profileIconContainer1.classList.add("d-none");
-        profileIconContainer2.classList.remove("d-none");
     } else if (window.innerWidth <= 992 && window.innerWidth > 576) {
         navbar_toggler.hidden = false;
         navbar_nav.classList.add("navbar-collapse", "d-none");
-        profileIconContainer1.classList.remove("d-none");
-        profileIconContainer2.classList.add("d-none");
     } else {
         navbar_toggler.hidden = false;
         navbar_nav.classList.add("navbar-collapse", "d-none");
-        profileIconContainer1.classList.add("d-none");
-        profileIconContainer2.classList.remove("d-none");
     }
 }
 
